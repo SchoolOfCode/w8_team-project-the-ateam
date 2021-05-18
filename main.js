f6e3f7cefa1a3df7f17c67e73b04865520cb467d
Becks Maybury <becksmaybury@hotmail.co.uk>
2021-05-18 22:18:02 +0100

    bing api basics working, no category

---
+++ main.js
@@ -0,0 +1,39 @@
+
+let horoscope = "";
+let sign = "scorpio";
+let newsHeadlines = "test";
+
+
+var requestOptions = {
+    method: 'GET',
+    headers: {
+          "Content-Type":"application/json; charset=UTF-8"},
+    redirect: 'follow'
+  };
+
+async function fetchHoroscope () {
+    let response = await fetch(`https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${sign}&day=today`, {
+        "method": "POST",
+        "headers": {
+            "x-rapidapi-key": "c6b7faf605msha0634e3b1f07049p1d0dbdjsn61e10d597152",
+            "x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com"
+        }
+    }
+    )
+    horoscope = await response.json();
+    return horoscope;
+}
+
+async function fetchNewsHeadlines () {
+    let response = await fetch("https://bing-news-search1.p.rapidapi.com/news?textFormat=Raw&safeSearch=Off&setLang=en-GB", {
+        "method": "GET",
+        "headers": {
+            "x-bingapis-sdk": "true",
+            "x-rapidapi-key": "c6b7faf605msha0634e3b1f07049p1d0dbdjsn61e10d597152",
+            "x-rapidapi-host": "bing-news-search1.p.rapidapi.com"
+        }
+    }
+    )
+    newsHeadlines = await response.json();
+    return newsHeadlines;
+}

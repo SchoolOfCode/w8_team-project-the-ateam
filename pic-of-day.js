4e43cf7b5209b8bde93b813bf576f6c8f4a0d6f8
merlinjones10 <merlinjones10@gmail.com>
2021-05-20 16:15:45 +0100

    background choice added

--- pic-of-day.js
+++ pic-of-day.js
@@ -2,12 +2,13 @@
 
 async function getBackground() {
   const NasaApiKey = "d1I88fYYUtxlURwBFr15pmDXpsIb0sAADqcKSHnh";
-  let date = "2020-05-05";
-  let apiLink = `https://api.nasa.gov/planetary/apod?api_key=${NasaApiKey}&date=${date}`;
+  let backgrounds = ["2020-04-05", "2020-02-04", "2020-04-06"];
+  let apiLink = `https://api.nasa.gov/planetary/apod?api_key=${NasaApiKey}&date=${backgrounds[1]}`;
   const response = await fetch(apiLink);
   const obj = await response.json();
   let imageLink = obj.url;
   console.log(imageLink);
   document.body.style.backgroundImage = `url(${imageLink})`;
+  console.log(obj);
 }
 getBackground();

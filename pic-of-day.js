6dc13ae55e8a1ec8fa67f7604a7c4f76da7c31f9
merlinjones10 <merlinjones10@gmail.com>
2021-05-20 16:04:37 +0100

    background image of day working

--- pic-of-day.js
+++ pic-of-day.js
@@ -1,1 +1,13 @@
 // pic of day
+
+async function getBackground() {
+  const NasaApiKey = "d1I88fYYUtxlURwBFr15pmDXpsIb0sAADqcKSHnh";
+  let date = "2020-05-05";
+  let apiLink = `https://api.nasa.gov/planetary/apod?api_key=${NasaApiKey}&date=${date}`;
+  const response = await fetch(apiLink);
+  const obj = await response.json();
+  let imageLink = obj.url;
+  console.log(imageLink);
+  document.body.style.backgroundImage = `url(${imageLink})`;
+}
+getBackground();

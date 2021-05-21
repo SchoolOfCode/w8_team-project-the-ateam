15e035091d3785db34e2f036267de14c28198319
katehrsn <kate.hrsn@gmail.com>
2021-05-21 15:42:41 +0100

    added search bar but it doesnt work lol

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// modal box 
 const userInfoBox = document.querySelector("#user-info-box");
 const userInfoBoxOpenButton = document.querySelector("#change-user-info-button");
 const userInfoBoxCloseButton = document.querySelector("#close-modal");
@@ -9,11 +10,31 @@ function openModal() {
     userInfoBox.style.display = "block";
 }
 function closeModal() {
-    userInfoBox.style.display = "none"
+    userInfoBox.style.display = "none";
 }
 
+// search bar
+const searchBar = document.querySelector("#search-bar");
+const userSearch = document.querySelector("#user-search");
 
+let userSearchQuery = userSearch.value 
 
+function convertUserSearch(word) {
+    return word.replace(` `,`+`);
+    // only replaces first instance of " " with "+" deffo needs changing
+}
+
+searchBar.addEventListener("submit", googleSomething);
+
+function googleSomething() {
+    let search = convertUserSearch(userSearchQuery);
+    searchBar.action = `https://www.google.com/search?q=${search}`;
+    searchBar.target = "_blank";
+    // only opens google homepage???
+
+}
+
+// horoscope
 let horoscope = "";
 let sign = "scorpio";
 

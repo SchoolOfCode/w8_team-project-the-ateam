bc676eefbed2fd49cb9e61e46fab04f49e0baeac
Becks Maybury <becksmaybury@hotmail.co.uk>
2021-05-24 18:41:24 +0100

    resolved issue with forecast dropdown changing after user submit

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ let userInfoBox = document.querySelector("#user-info-box");
 let userInfoBoxOpenButton = document.querySelector("#change-user-info-button");
 let userInfoBoxCloseButton = document.querySelector("#close-modal");
 let userDetailsButton = document.getElementById("change-user-info");
+let backgroundImageButton = document.getElementById("info-button");
 let forecastDropdown = document.getElementById("forecast-dropdown");
 let newsTopicsDropdown1 = document.getElementById("topics-1-dropdown");
 
@@ -80,7 +81,10 @@ function setUserDetails (event) {
     if (userDetails.name !== "Friend") {
      userInfoBoxOpenButton.classList.remove("heartbeat");
      }
+     if (forecastDropdown.value === "weather") {
     weatherDisplay();
+     }
+     horoscopeDisplay();
 }
 
 // DROPDOWN BOXES FUNCTIONS
@@ -201,6 +205,7 @@ async function weatherDisplay () {
       weatherImage.src = `${weatherDetails.current.condition.icon}`;
       weatherTemperature.innerText = `${weatherDetails.current.temp_c}°C`;
       weatherLocation.innerText = `${weatherDetails.location.name}`;
+      forecastDropdown.value = "weather";
   }
 
 async function newsItemsDisplay () {
@@ -229,6 +234,9 @@ async function horoscopeDisplay () {
     let horoscopeDetails = await fetchHoroscope();
     horoscopeImage.src = `/images/${userDetails.starsign}.png`;
     horoscopeDescription.innerText = `"${horoscopeDetails.description}"`;  
+    let horoscopeTitle = document.getElementById("horoscope-sign");
+    horoscopeTitle.innerText = userDetails.starsign;
+    forecastDropdown.value = "horoscope";
   }
 
 function clearWeatherDisplay () {
